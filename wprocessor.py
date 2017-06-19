0bc199f8339152bb4dd26a66af522cf232fd9635
Xicoténcatl Delgado Maya <demxic@gmail.com>
2017-06-19 18:02:08 -0500

    feat : Add a new Cursor clas

--- wprocessor.py
+++ wprocessor.py
@@ -1,24 +1,42 @@
+class Cursor(object):
+	"""Basic cursor that knows its position and moves itsel relative to it"""
+	def __init__(self, document):
+		self.document = document
+		self.position = 0
+
+	def forward(self):
+		self.position += 1
+
+	def back(self):
+		self.position -= 1
+
+	def home(self):
+		while self.document.characters[self.position-1] !='\n' :
+			self.back()
+			if self.position == 0:
+				#Got to beginning of file before new line
+				break
+
+	def end(self):
+		while self.position < len(self.document.characters) and self.document.characters[self.position] !='\n':
+			self.forward()
+
 class Document(object):
 	"""Basic document Class"""
 	def __init__(self):
 		self.characters = []
-		self.cursor = 0
+		self.cursor = Cursor(self)
 		self.filename = ''
 
 	def insert(self,character):
-		self.characters.insert(self.cursor, character)
-		self.cursor += 1
+		self.characters.insert(self.cursor.position, character)
+		self.cursor.forward()
 
 	def delete(self):
-		del self.characters[self.cursor]
+		del self.characters[self.cursor.position]
 
 	def save(self):
 		f = open(self.filename, 'w')
 		f.write("".join(self.characters))
 		f.close()
 
-	def foward(self):
-		self.cursor += 1
-
-	def back(self):
-		self.cursor -= 1
