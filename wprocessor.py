47bc07cc1d9ef56341f7a198c3d0d923820bb3e7
demxic <demxic@gmail.com>
2017-06-28 00:09:44 -0500

    feat: Add the Character class

--- wprocessor.py
+++ wprocessor.py
@@ -11,14 +11,14 @@ class Cursor(object):
 		self.position -= 1
 
 	def home(self):
-		while self.document.characters[self.position-1] !='\n' :
+		while self.document.characters[self.position-1].character !='\n' :
 			self.back()
 			if self.position == 0:
 				#Got to beginning of file before new line
 				break
 
 	def end(self):
-		while self.position < len(self.document.characters) and self.document.characters[self.position] !='\n':
+		while self.position < len(self.document.characters) and self.document.characters[self.position].character !='\n':
 			self.forward()
 
 class Document(object):
@@ -29,6 +29,8 @@ class Document(object):
 		self.filename = ''
 
 	def insert(self,character):
+		if not hasattr(character, 'character'):
+			character = Character(character)
 		self.characters.insert(self.cursor.position, character)
 		self.cursor.forward()
 
@@ -42,5 +44,19 @@ class Document(object):
 
 	@property
 	def string(self):
-		return "".join(self.characters)
+		return "".join(str(c) for c in self.characters)
+
+class Character(object):
+	def __init__(self, character, bold=False, italic=False, underline=False):
+		assert len(character)==1
+		self.character = character
+		self.bold = bold
+		self.italic = italic
+		self.underline = underline
+
+	def __str__(self):
+		bold = "*" if self.bold else ''
+		italic = "/" if self.italic else ''
+		underline = "_" if self.underline else ''
+		return bold + italic + underline + self.character
 
